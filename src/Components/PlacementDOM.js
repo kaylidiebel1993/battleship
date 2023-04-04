7e5935f3189c48dca33fa8693c074f1ef097ed54
Kayli <kayli.diebel@gmail.com>
2023-04-04 13:23:53 -0700

    Write logic for PlacementDOM

---
+++ src/Components/PlacementDOM.js
@@ -0,0 +1,47 @@
+import React from "react"; 
+
+const PlacementDOM = (props) => {
+    return (
+        <div id="interface">
+            <button className="interface-btn" onClick={props.toggleVertical}>
+                Vertical 
+            </button>
+            <button className="interface-btn" id="carrier" onClick={props.changeShip}>
+                Carrier
+            </button>
+            <button 
+            classname="interface-btn" 
+            id="battleship"
+            onClick={props.changeShip}
+            >
+                Battleship
+            </button>
+            <button className="interface-btn" id="cruiser" onClick={props.changeShip}>
+                Cruiser 
+            </button>
+            <button 
+            className="interface-btn"
+            id="submarine"
+            onClick={props.changeShip}
+            >
+                Submarine 
+            </button>
+            <button 
+            className="interface-btn"
+            id="destroyer"
+            onClick={props.changeShip} 
+            >
+                Destroyer 
+            </button>
+            <button 
+            className="interface-btn"
+            id="start-game-btn"
+            onClick={props.startGame}
+            >
+                Start Game 
+            </button>
+        </div>
+    );
+}; 
+
+export default PlacementDOM; 
